eacc6aa7efdb5b36071e923a52046d6d65947a2d
cletus-cyber <114840476+cletus-cyber@users.noreply.github.com>
2023-04-02 15:36:39 +0000

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+#Area of a square
+#A square is a plane figure with all it's sides equal
+#A function to calculate the area of a square is given below,
+
+
+def area_of_square():
+    length = float(input("Enter length of square: "))
+    area = length**2
+    print("The area of the square is:",area,"unit sq")
+
+area_of_square()
